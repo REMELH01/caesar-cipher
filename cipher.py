09d6f407133fdd7f2debe89370c14102422e6067
REMELH01 <rmelhornmusic@gmail.com>
2024-11-17 12:50:44 -0500

    added cipher code

--- cipher.py
+++ cipher.py
@@ -1,1 +1,17 @@
-# add your code here
+def caesar_cipher(text, shift):
+    encrypted_text = ""
+
+    for char in text:
+        if char.isalpha():
+            char = char.lower()
+            new_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            encrypted_text += new_char
+        else:
+            encrypted_text += char
+
+    return encrypted_text
+
+user_input = input("Please enter a sentence: ")
+encrypted_output = caesar_cipher(user_input, 5)
+
+print("The encrypted sentence is:", encrypted_output)
